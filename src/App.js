2e3e8337d50553b649e2281e229da29611631832
jkt48 <isihardisk21@gmail.com>
2019-01-03 13:09:27 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -5,44 +5,68 @@ import { Vocabulary } from './components/Vocabulary';
 import { Grammer } from './components/Grammer';
 import { Listening } from './components/Listening';
 import {Topics} from './components/Testing-Pages/nestedPath'
-import { ComunicateToServer as Server} from './components/Testing-Pages/comunicateToServer'
+import { ComunicateToServer as Server } from './components/Testing-Pages/comunicateToServer';
+import { Nav, NavItem, NavLink, Container } from 'reactstrap';
+
 
 class App extends Component {
-  state = {
-    data: 'cabe'
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: 'cabe',
+      dropdownOpen: false
+    }
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+ 
+  addActiveClass() {
+    console.log(window.location);
   }
   componentDidMount() {
-  }
-  readPath() {
-   
-  }
-  readFs() {
-    
+    this.addActiveClass();
   }
   render() {
     const { data } = this.state;    
     return (
       <div>
-        <ul>
-          <li>
-            <Link to="/">Vocabulary</Link>
-          </li>
-          <li>
-            <Link to="/grammer">Grammer</Link>
-          </li>
-          <li>
-            <Link to="/listening">Listening</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li>
-          <li>
-            <Link to="/server">Server</Link>
-          </li>
-        </ul>
-
-        <hr />
-
+        <br/>
+        <Container>
+          <Nav pills>
+            <NavItem>
+              <NavLink href="/grammer" active={true}>Vocabulary -Notes</NavLink>
+              {/* <Link
+                className="nav-link active"
+                to="#">Vocabulary -Notes</Link> */}
+            </NavItem>
+          </Nav>
+          <Nav tabs className="justify-content-center">
+            <NavItem>
+              <Link
+                className="nav-link active"
+                to="/">Vocabulary</Link>
+            </NavItem>
+            <NavItem>
+              <Link className="nav-link" to="/grammer">Grammer</Link>
+            </NavItem>
+            <NavItem>
+              <Link className="nav-link" to="/listening">Listening</Link>
+            </NavItem>
+            <NavItem>
+              <Link className="nav-link" to="/topics">Topics</Link>
+            </NavItem>
+            <NavItem>
+              <Link className="nav-link" to="/server">Server</Link>
+            </NavItem>
+          </Nav>
+        </Container>
+        
+        
         <Route exact path="/" render={(props) => <Vocabulary {...props}  data={data}/>} />
         <Route path="/grammer" component={Grammer} />
         <Route path="/listening" component={Listening} />
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'onsenui/css/onsenui.css';
-import 'onsenui/css/onsen-css-components.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom'
 
